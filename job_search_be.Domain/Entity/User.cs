bda6a40da51f1e5ad9503d65e53f39f9c38355d6
PhamHuy <phamkhachuy240702@gmail.com>
2024-03-21 04:26:30 +0700

    first commit

---
+++ job_search_be.Domain/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job_search_be.Domain.Entity
+{
+    internal class User
+    {
+    }
+}
